eaa23453a529cf6fff84f71cb68bd67237b1a859
Pouria Seyfi <seyfipouria@gmail.com>
2025-05-16 10:47:36 +0330

    feat: upload and storage

---
+++ adapter/postgresql2/adapter.go
@@ -0,0 +1,43 @@
+package postgresql2
+
+import "database/sql"
+
+import (
+	_ "github.com/lib/pq"
+)
+
+type Adapter struct {
+	db *sql.DB
+}
+
+func New(connStr string) (*Adapter, error) {
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return &Adapter{db: db}, nil
+}
+
+// Exec executes a query that modifies data (e.g., INSERT, UPDATE, DELETE).
+func (a *Adapter) Exec(query string, args ...interface{}) (sql.Result, error) {
+	return a.db.Exec(query, args...)
+}
+
+// Query executes a query that returns rows (e.g., SELECT).
+func (a *Adapter) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	return a.db.Query(query, args...)
+}
+
+// QueryRow executes a query that returns at most one row.
+func (a *Adapter) QueryRow(query string, args ...interface{}) *sql.Row {
+	return a.db.QueryRow(query, args...)
+}
+
+func (a *Adapter) Close() error {
+	return a.db.Close()
+}
--- filer/adapter/tusdadapter/adapter.go
+++ filer/adapter/tusdadapter/adapter.go
@@ -1,32 +1,64 @@
 package tusdadapter
 
 import (
+	"errors"
+	"github.com/gocastsian/roham/filer/adapter/s3adapter"
 	"github.com/tus/tusd/pkg/filestore"
 	tusd "github.com/tus/tusd/pkg/handler"
 	"github.com/tus/tusd/pkg/s3store"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
 )
 
-type PreUploadCreateCallback func(hook tusd.HookEvent) error
-
-type UploadEvent struct {
+type UploadValidator interface {
+	ValidateUpload(targetBucket string, size int64) error
 }
 
-type Adapter struct {
+func New() *tusd.Handler {
+	// Set up tusd file store
+	store := filestore.FileStore{
+		Path: "./uploads/",
+	}
+
+	// Create tusd handler config
+	composer := tusd.NewStoreComposer()
+	store.UseIn(composer)
+
+	h, err := tusd.NewHandler(tusd.Config{
+		BasePath:                "/files/",
+		StoreComposer:           composer,
+		RespectForwardedHeaders: true,
+	})
+	if err != nil {
+		log.Fatalf("Failed to create tusd handler: %v", err)
+	}
+	return h
 }
 
-func NewWithFileStore(store filestore.FileStore, callback PreUploadCreateCallback) (*tusd.Handler, error) {
+func NewHandlerWithS3Store(bucketName string, v UploadValidator, adapter *s3adapter.Adapter) (*tusd.Handler, error) {
 
 	tusd.NewStoreComposer()
+	store := s3store.New(bucketName, adapter.S3())
 
-	// Create a new tusd handler with the file store
 	composer := tusd.NewStoreComposer()
 	store.UseIn(composer)
 
 	handler, err := tusd.NewHandler(tusd.Config{
-		BasePath:                "/uploads/",
-		StoreComposer:           composer,
+		BasePath:      "/uploads/",
+		StoreComposer: composer,
+		//PreUploadCreateCallback: func(hook tusd.HookEvent) error {
+		//	targetBucket := hook.HTTPRequest.Header.Get("Bucket")
+		//	if targetBucket == "" {
+		//		return errors.New("bucket name is required")
+		//	}
+		//	return v.ValidateUpload(targetBucket, hook.Upload.Size)
+		//},
 		NotifyCompleteUploads:   true,
-		PreUploadCreateCallback: callback,
+		NotifyTerminatedUploads: true,
+		NotifyUploadProgress:    true,
+		NotifyCreatedUploads:    true,
 	})
 
 	if err != nil {
@@ -36,20 +68,44 @@ func NewWithFileStore(store filestore.FileStore, callback PreUploadCreateCallbac
 	return handler, err
 }
 
-func NewWithS3Store(store s3store.S3Store, callback PreUploadCreateCallback) (*tusd.Handler, error) {
+func NewHandlerWithFileStore(bucketName string, v UploadValidator) (*tusd.Handler, error) {
 
-	tusd.NewStoreComposer()
+	store := filestore.New("./uploads/" + bucketName)
 
-	// Create a new tusd handler with the file store
 	composer := tusd.NewStoreComposer()
 	store.UseIn(composer)
 
+	//todo get cors from configs. cors should be handle by proxy or tusd handle?
+
 	handler, err := tusd.NewHandler(tusd.Config{
-		BasePath:                "/uploads/",
-		StoreComposer:           composer,
-		NotifyCompleteUploads:   true,
-		PreUploadCreateCallback: callback,
+		BasePath: "/uploads/",
+		Cors: &tusd.CorsConfig{
+			AllowOrigin:   regexp.MustCompile(".*"),
+			AllowHeaders:  "*",
+			ExposeHeaders: "*",
+			AllowMethods:  "*",
+		},
+		StoreComposer:         composer,
+		NotifyCompleteUploads: true,
+		PreUploadCreateCallback: func(hook tusd.HookEvent) error {
+
+			targetBucket := hook.HTTPRequest.Header.Get("Bucket")
+			if targetBucket == "1" {
+				return tusd.NewHTTPError(errors.New("missing required header: Bucket"), http.StatusUnprocessableEntity)
+			}
+			return v.ValidateUpload(targetBucket, hook.Upload.Size)
+		},
+		NotifyTerminatedUploads: true,
+		NotifyUploadProgress:    true,
+		NotifyCreatedUploads:    true,
 	})
+
+	// Create uploads dir if not exist
+	if _, err := os.Stat("./uploads/" + bucketName); os.IsNotExist(err) {
+		if err := os.Mkdir("./uploads/"+bucketName, os.ModePerm); err != nil {
+			log.Fatalf("Could not create upload dir: %v", err)
+		}
+	}
 
 	if err != nil {
 		return nil, err
